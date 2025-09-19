490f0855ed3537105202d13ff8bbebfaa55c6f74
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2025-09-18 21:06:59 -0700

    fix: Static files have a different path between local dev / and gh pages /koan.e can get away with src being relative in our data.js files, without the root /, i.e. 'image.jpg'.
    However, when the image is referenced from CSS the relative path includes /assets as it's relative to the css file. I'm tryiing to avoid additional code to manage conditional paths for gh-pages.

--- stories/Molecules/Card/Card.data.js
+++ stories/Molecules/Card/Card.data.js
@@ -55,7 +55,7 @@ export default {
   overlay: {
     variant: "overlay",
     media: {
-      url: `image.jpg`,
+      url: `../image.jpg`,
     },
     heading: "Heading For A Card",
     text: "<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare. Ac non lacus sollicitudin vulputate sociosqu.</p>",
